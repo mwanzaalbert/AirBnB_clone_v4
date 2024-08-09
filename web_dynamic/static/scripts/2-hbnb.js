5bccceb1dbfdff387e4817093ab9c8151f270e7f
mwanzaalbert <user.email>
2024-08-09 10:35:49 -0700

    task 3 completed

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    // Check API status
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+
+    // Handle amenity selection (same as 1-hbnb.js)
     const selectedAmenities = {};
 
     $('input[type="checkbox"]').change(function () {
@@ -15,3 +25,4 @@ $(document).ready(function () {
         $('.amenities h4').text(amenityList);
     });
 });
+
